a337782ddd8a500a71ebf889576ab60288655915
dai sy <516093398@qq.com>
2016-07-09 16:31:33 +0800

    2

---
+++ KBCC/Areas/Customer/Controllers/LogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KBCC.Areas.Customer.Controllers
+{
+    public class LogController : Controller
+    {
+        // GET: Customer/Log
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
